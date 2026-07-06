13b7fa39e1030c91126ff03f71c736d0540cfe52
codesam <tedsamhain@gmail.com>
2026-07-06 00:05:43 +0000

    nav: fix Home link NavigationDuplicated by using click handler instead of router-link
    Scrolls to top on click; only navigates to timeline if not already there.

--- frontend/src/components/Page/PageLayout/NavPanel.vue
+++ frontend/src/components/Page/PageLayout/NavPanel.vue
@@ -2,7 +2,7 @@
   <nav class="nav-panel no-select" :class="{ 'nav-panel--open': showNavPanel }">
     <div v-show="showNavPanel" class="nav-panel__inner">
       <v-list nav :key="route.fullPath" :disabled="!initializedStore.initialized">
-        <v-list-item slim to="/" prepend-icon="mdi-home" title="Home" />
+        <v-list-item slim prepend-icon="mdi-home" title="Home" @click="goHome" />
         <v-list-item slim to="/albums" prepend-icon="mdi-image-album" title="Albums" />
         <v-list-item slim to="/tags" prepend-icon="mdi-tag-multiple" title="Tags" />
       </v-list>
@@ -18,12 +18,20 @@
 
 <script setup lang="ts">
 import { inject, type Ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useInitializedStore } from '@/store/initializedStore'
 
 const showNavPanel = inject<Ref<boolean>>('showNavPanel')
 const route = useRoute()
+const router = useRouter()
 const initializedStore = useInitializedStore('mainId')
+
+const goHome = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+  if (route.name !== 'timeline') {
+    void router.push({ name: 'timeline' })
+  }
+}
 </script>
 
 <style scoped>
